0e806a1ad0f16a8b713e5ca3b9a91a3becee3ea9
Vinicius Paranho Ribeiro <viniciusparanho@gmail.com>
2024-02-26 12:12:13 -0300

    Add files via upload

---
+++ PRJ_HELLOWORLD/src/PRJ_HELLOWORLD.c
@@ -0,0 +1,28 @@
+/*
+ ============================================================================
+ Name        : PRJ_HELLOWORLD.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#define PI 3.141559
+
+int main(void) {
+
+	int idade = 26;
+	const int MAX_NUM = 100;
+
+			printf("Minha variável int comum = %d\n", idade);
+			printf("Minha variável int constante = %d\n", MAX_NUM);
+			printf("Minha constante  = %f\n", PI);
+
+			//MAX_NUM = 99
+
+			return EXIT_SUCCESS;
+
+}
